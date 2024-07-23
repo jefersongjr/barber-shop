6edb389a28aabedbe4514f2baefcf2ef007ab740
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-23 17:53:30 -0300

    :tada: Initial commit :tada:

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { somar } from '@barber/core';
+
+export default function Home() {
+  return (
+    <main>
+      {somar(23, 42)}
+    </main>
+  );
+}
---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+export default function App() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>App</View>
+    );
+}
